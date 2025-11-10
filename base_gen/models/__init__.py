a5d0c7b5ed200018e2519ef5bb5ea6b8ed9bd563
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-10 15:18:54 +0100

    [MIG] Migrating __init__

--- base_gen/models/__init__.py
+++ base_gen/models/__init__.py
@@ -1,8 +1,4 @@
 # 2024 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from . import common_format
-from . import common_image
-from . import common_log
-from . import common_metadata
-from . import simple_model
+from . import common_format, common_image, common_log, common_metadata, simple_model
