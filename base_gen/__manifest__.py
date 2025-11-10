fdd24f684defee0239306afda1300b33380f1359
César Andrés Sánchez <rasec.andres@gmail.com>
2025-11-10 14:58:41 +0100

    [MIG] base_gen/: Migration to 18.0

--- base_gen/__manifest__.py
+++ base_gen/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Base-General Module",
     "summary": "General purpose tools for any module",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Moval Agroingeniería",
     "license": "AGPL-3",
     "website": "https://moval.es",
