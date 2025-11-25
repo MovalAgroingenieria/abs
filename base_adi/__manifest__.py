905df5cf9a06a53992ac45bfaf63505ae0352a7a
Git Moval <git@moval.es>
2025-11-26 00:10:34 +0100

    [MIG] base_adi: Migration to 17.0

--- base_adi/__manifest__.py
+++ base_adi/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Administrative Divisions",
     "summary": "Management of a country's administrative divisions",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "author": "Moval Agroingeniería",
     "license": "AGPL-3",
     "website": "https://moval.es",
