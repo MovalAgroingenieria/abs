a3295e4180a18d5dcf087baf1dcfd61b0a604be2
Git Moval <git@moval.es>
2025-11-26 00:10:34 +0100

    [MIG] base_gis: Migration to 17.0

--- base_gis/__manifest__.py
+++ base_gis/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Base-GIS Module",
     "summary": "Additional GIS tools",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Moval Agroingeniería",
     "license": "AGPL-3",
     "website": "https://moval.es",
