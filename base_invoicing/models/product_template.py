6c7706c29e3fb4fb27d6b67b0d54af26db7b6c59
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-19 12:21:27 +0100

    [18.0][MIG] base_invoicing: migrate product.template mass billing helpers

--- base_invoicing/models/product_template.py
+++ base_invoicing/models/product_template.py
@@ -1,27 +1,31 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = ['product.template']
+    _inherit = "product.template"
 
     factor_quantity = fields.Float(
-        string='Factor applicable to the quantity of product',
+        string="Factor applicable to the quantity of product",
         digits=(32, 4),
-        default=1,
-        required=True,)
-
+        default=1.0,
+        required=True,
+    )
     link_with_billable_items = fields.Boolean(
-        string='Link product with its billable items (y/n)',
-        default=False,)
-
+        string="Link product with its billable items (y/n)",
+        default=False,
+    )
     supports_mass_billing = fields.Boolean(
-        string='Supports massive billing (y/n)',
-        related='categ_id.supports_mass_billing',)
+        string="Supports massive billing (y/n)",
+        related="categ_id.supports_mass_billing",
+    )
 
     _sql_constraints = [
-        ('factor_quantity_ok', 'CHECK (factor_quantity >= 0)',
-         'Incorrect value for "Factor applicable to the quantity of product".'),
+        (
+            "factor_quantity_ok",
+            "CHECK (factor_quantity >= 0)",
+            'Incorrect value for "Factor applicable to the quantity of product".',
+        ),
     ]
