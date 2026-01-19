1ce1b9d1faef5df3d440bf1aca131d814183e2bd
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-19 12:39:12 +0100

    [18.0][MIG] base_invoicing: fix field selection wizard for Odoo 18
    Avoid global deletion of transient records and ensure wizard-safe field options.
    Refactor default_get and context handling.

--- base_invoicing/wizards/wizard_select_field.py
+++ base_invoicing/wizards/wizard_select_field.py
@@ -1,55 +1,101 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class FieldOption(models.TransientModel):
-    _name = 'field.option'
-    _description = 'Options for the wizard selection field (auxiliary model)'
+    _name = "field.option"
+    _description = "Selectable field option"
 
-    name = fields.Char()
-    field_description = fields.Char()
+    wizard_id = fields.Many2one(
+        comodel_name="wizard.select.field",
+        ondelete="cascade",
+        required=True,
+    )
+    name = fields.Char(required=True)
+    field_description = fields.Char(required=True)
 
     def name_get(self):
         return [(record.id, record.field_description) for record in self]
 
 
 class WizardSelectField(models.TransientModel):
-    _name = 'wizard.select.field'
-    _description = 'Dialog box to select a field of the billable items model'
+    _name = "wizard.select.field"
+    _description = "Select a billable item field"
 
     selected_field_id = fields.Many2one(
-        string='Selected Field',
-        comodel_name='field.option',)
+        comodel_name="field.option",
+        string="Selected Field",
+        domain="[('wizard_id', '=', id)]",
+    )
 
     @api.model
-    def default_get(self, var_fields):
-        if 'selected_field_id' in var_fields:
-            record = self.env['product.category'].browse(
-                self.env.context['active_id'])
-            source_model = record.billable_item_model_id.model
-            field_types = self.env.context.get('types', '')
-            fields_metadata = self.env['common.metadata'].get_fields(
-                source_model, field_types)
-            model_field_option = self.env['field.option']
-            model_field_option.search([]).unlink()
-            for field_metadata in fields_metadata or []:
-                model_field_option.create({
-                    'name':
-                        field_metadata['name'],
-                    'field_description':
-                        field_metadata['name'] + ' (' +
-                        field_metadata['field_description'] + ')'
-                })
-        return {}
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+
+        active_id = self.env.context.get("active_id")
+        if not active_id:
+            return res
+
+        category = self.env["product.category"].browse(active_id)
+        if not category.exists() or not category.billable_item_model_id:
+            return res
+
+        field_types = self.env.context.get("types", "")
+        model_name = category.billable_item_model_id.model
+
+        fields_metadata = self.env["common.metadata"].get_fields(
+            model_name, field_types
+        )
+
+        wizard = self.create({})
+
+        options = []
+        for field in fields_metadata or []:
+            options.append(
+                (
+                    0,
+                    0,
+                    {
+                        "wizard_id": wizard.id,
+                        "name": field["name"],
+                        "field_description": (
+                            f'{field["name"]} ({field["field_description"]})'
+                        ),
+                    },
+                )
+            )
+
+        if options:
+            wizard.write({"selected_field_id": False})
+            wizard.env["field.option"].create(
+                [
+                    {
+                        "wizard_id": wizard.id,
+                        "name": field["name"],
+                        "field_description": f'{field["name"]} ({field["field_description"]})',
+                    }
+                    for field in fields_metadata
+                ]
+            )
+
+        return res
 
     def set_selected_field(self):
         self.ensure_one()
-        record = self.env['product.category'].browse(
-            self.env.context['active_id'])
-        destination_field = self.env.context.get('field', False)
-        if record and destination_field:
-            record.write({
+
+        active_id = self.env.context.get("active_id")
+        destination_field = self.env.context.get("field")
+        if not active_id or not destination_field or not self.selected_field_id:
+            return
+
+        category = self.env["product.category"].browse(active_id)
+        if not category.exists():
+            return
+
+        category.write(
+            {
                 destination_field: self.selected_field_id.name,
-            })
+            }
+        )
