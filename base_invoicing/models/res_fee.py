ab6b97dd16190a84f42669122f2eb7cf9640c8f4
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-19 12:23:47 +0100

    [18.0][MIG] base_invoicing: simplify res.fee partner mirror fields

--- base_invoicing/models/res_fee.py
+++ base_invoicing/models/res_fee.py
@@ -1,115 +1,61 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class ResFee(models.Model):
-    _name = 'res.fee'
-    _description = 'Fee'
-    _inherits = {'res.partner': 'partner_id'}
-    _inherit = ['account.billable.item']
+    _name = "res.fee"
+    _description = "Fee"
+    _inherits = {"res.partner": "partner_id"}
+    _inherit = "account.billable.item"
 
-    _billing_partner_id_name = 'partner_id'
-    _billing_quantity_name = ''
+    _billing_partner_id_name = "partner_id"
+    _billing_quantity_name = ""
 
     partner_id = fields.Many2one(
-        string='Associated Partner',
-        comodel_name='res.partner',
+        comodel_name="res.partner",
+        string="Associated Partner",
         required=True,
-        ondelete='cascade',
+        ondelete="cascade",
         auto_join=True,
-        index=True,)
+        index=True,
+    )
 
     partner_parent_id = fields.Many2one(
-        string='Related company of partner',
-        comodel_name='res.partner',
+        comodel_name="res.partner",
+        string="Related company of partner",
+        related="partner_id.parent_id",
         store=True,
-        compute='_compute_partner_parent_id',
-        index=True, )
-
+        index=True,
+    )
     partner_partner_share = fields.Boolean(
-        string='Share partner of partner',
+        string="Share partner of partner",
+        related="partner_id.partner_share",
         store=True,
-        compute='_compute_partner_partner_share',)
-
+    )
     partner_vat = fields.Char(
-        string='VAT of partner',
+        string="VAT of partner",
+        related="partner_id.vat",
         store=True,
-        compute='_compute_partner_vat',)
-
+    )
     partner_email = fields.Char(
-        string='Email of partner',
+        string="Email of partner",
+        related="partner_id.email",
         store=True,
-        compute='_compute_partner_email',)
-
+    )
     partner_phone = fields.Char(
-        string='Phone of partner',
+        string="Phone of partner",
+        related="partner_id.phone",
         store=True,
-        compute='_compute_partner_phone',)
-
+    )
     partner_mobile = fields.Char(
-        string='Mobile of partner',
+        string="Mobile of partner",
+        related="partner_id.mobile",
         store=True,
-        compute='_compute_partner_mobile',)
-
+    )
     partner_is_company = fields.Boolean(
-        string='Company (y/n)',
+        string="Company (y/n)",
+        related="partner_id.is_company",
         store=True,
-        compute='_compute_partner_is_company',)
-
-    @api.depends('partner_id', 'partner_id.parent_id')
-    def _compute_partner_parent_id(self):
-        for record in self:
-            partner_parent_id = None
-            if record.partner_id and record.partner_id.parent_id:
-                partner_parent_id = record.partner_id.parent_id
-            record.partner_parent_id = partner_parent_id
-
-    @api.depends('partner_id', 'partner_id.partner_share')
-    def _compute_partner_partner_share(self):
-        for record in self:
-            partner_partner_share = False
-            if record.partner_id and record.partner_id.partner_share:
-                partner_partner_share = True
-            record.partner_partner_share = partner_partner_share
-
-    @api.depends('partner_id', 'partner_id.vat')
-    def _compute_partner_vat(self):
-        for record in self:
-            partner_vat = None
-            if record.partner_id and record.partner_id.vat:
-                partner_vat = record.partner_id.vat
-            record.partner_vat = partner_vat
-
-    @api.depends('partner_id', 'partner_id.email')
-    def _compute_partner_email(self):
-        for record in self:
-            partner_email = None
-            if record.partner_id and record.partner_id.email:
-                partner_email = record.partner_id.email
-            record.partner_email = partner_email
-
-    @api.depends('partner_id', 'partner_id.phone')
-    def _compute_partner_phone(self):
-        for record in self:
-            partner_phone = None
-            if record.partner_id and record.partner_id.phone:
-                partner_phone = record.partner_id.phone
-            record.partner_phone = partner_phone
-
-    @api.depends('partner_id', 'partner_id.mobile')
-    def _compute_partner_mobile(self):
-        for record in self:
-            partner_mobile = None
-            if record.partner_id and record.partner_id.mobile:
-                partner_mobile = record.partner_id.mobile
-            record.partner_mobile = partner_mobile
-
-    @api.depends('partner_id', 'partner_id.is_company')
-    def _compute_partner_is_company(self):
-        for record in self:
-            partner_is_company = False
-            if record.partner_id and record.partner_id.is_company:
-                partner_is_company = True
-            record.partner_is_company = partner_is_company
+    )
