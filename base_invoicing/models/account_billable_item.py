daaa2a9252d926ebfd6a9c98b8a102ef352fbf97
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-19 12:13:14 +0100

    [18.0][MIG] base_invoicing: refactor account.billable.item for OCA style
    * Align imports/formatting with OCA conventions.
    * Simplify compute methods and return recordsets where appropriate.
    * Remove unused variables and harden getattr usage.

--- base_invoicing/models/account_billable_item.py
+++ base_invoicing/models/account_billable_item.py
@@ -1,89 +1,77 @@
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountBillableItem(models.AbstractModel):
-    _name = 'account.billable.item'
-    _description = ('Abstract model inherited by any concrete model '
-                    'that contains billable records')
+    _name = "account.billable.item"
+    _description = "Abstract model for billable records"
 
-    # Name of the field in the billable items model that serves as the
-    # reference to the partner.
-    _billing_partner_id_name = 'partner_id'
-
-    # Name of the field in the billable items model that plays the role
-    # of quantity.
-    _billing_quantity_name = 'quantity'
-
-    # Name of the field in the billable items model that plays the role
-    # of "additional grouping criterion".
-    _billing_groupvalue_name = ''
+    _billing_partner_id_name = "partner_id"
+    _billing_quantity_name = "quantity"
+    _billing_groupvalue_name = ""
 
     billing_partner_id = fields.Many2one(
-        string='Billing partner reference',
-        comodel_name='res.partner',
-        compute='_compute_billing_partner_id',)
-
+        comodel_name="res.partner",
+        compute="_compute_billing_partner_id",
+        string="Billing partner reference",
+    )
     billing_quantity = fields.Float(
-        string='Quantity field name',
+        compute="_compute_billing_quantity",
         digits=(32, 4),
-        compute='_compute_billing_quantity',)
-
+        string="Quantity field name",
+    )
     billing_groupvalue = fields.Char(
-        string='Grouping Value',
-        compute='_compute_billing_groupvalue',)
-
+        compute="_compute_billing_groupvalue",
+        string="Grouping Value",
+    )
     number_of_invoices = fields.Integer(
-        string='No. of invoices',
         default=0,
-        readonly=1,)
-
+        readonly=True,
+        string="No. of invoices",
+    )
     product_id = fields.Many2one(
-        string='Product',
-        comodel_name='product.product',
+        comodel_name="product.product",
         index=True,
-        ondelete='restrict',)
-
+        ondelete="restrict",
+        string="Product",
+    )
     move_line_ids = fields.One2many(
-        string='Invoice Lines',
-        comodel_name='account.move.line',
-        compute='_compute_move_line_ids',)
+        comodel_name="account.move.line",
+        compute="_compute_move_line_ids",
+        string="Invoice Lines",
+    )
 
+    @api.depends()
     def _compute_billing_partner_id(self):
         for record in self:
-            billing_partner_id = None
-            if self._billing_partner_id_name:
-                billing_partner_id = getattr(record,
-                                             self._billing_partner_id_name)
-            record.billing_partner_id = billing_partner_id
+            field_name = record._billing_partner_id_name
+            record.billing_partner_id = getattr(record, field_name, False) if field_name else False
 
+    @api.depends()
     def _compute_billing_quantity(self):
         for record in self:
-            billing_quantity = 1
-            if self._billing_quantity_name:
-                billing_quantity = getattr(record,
-                                           self._billing_quantity_name)
-            record.billing_quantity = billing_quantity
+            field_name = record._billing_quantity_name
+            record.billing_quantity = getattr(record, field_name, 1) if field_name else 1
 
+    @api.depends()
     def _compute_billing_groupvalue(self):
         for record in self:
-            billing_groupvalue = ''
-            if self._billing_groupvalue_name:
-                billing_groupvalue = str(
-                    getattr(record, self._billing_groupvalue_name))
-            record.billing_groupvalue = billing_groupvalue
+            field_name = record._billing_groupvalue_name
+            value = getattr(record, field_name, False) if field_name else False
+            record.billing_groupvalue = str(value) if value not in (False, None) else ""
 
+    @api.depends()
     def _compute_move_line_ids(self):
+        move_line_model = self.env["account.move.line"]
         for record in self:
-            move_line_ids = None
-            related_move_lines = self.env['account.move.line'].search(
-                [('billable_item_model', '=', self._name),
-                 ('billable_item_res_id', '=', record.id)])
-            if related_move_lines:
-                move_line_ids = related_move_lines.ids
-            record.move_line_ids = move_line_ids
+            record.move_line_ids = move_line_model.search(
+                [
+                    ("billable_item_model", "=", record._name),
+                    ("billable_item_res_id", "=", record.id),
+                ]
+            )
 
     @api.model
     def set_billing_quantity_name(self, quantity_name):
@@ -95,20 +83,12 @@ class AccountBillableItem(models.AbstractModel):
 
     @api.model
     def exists_active_field(self, model_name):
-        resp = False
-        active_field = self.env['common.metadata'].get_field(
-            model_name, 'active', exclude_related=True)
-        if active_field and active_field['ttype'] == 'boolean':
-            resp = True
-        return resp
+        field_meta = self.env["common.metadata"].get_field(
+            model_name, "active", exclude_related=True
+        )
+        return bool(field_meta and field_meta.get("ttype") == "boolean")
 
     @api.model
     def inherits_from_account_billable_item(self, model_name):
-        resp = False
-        inherited_models = self.env['common.metadata'].get_inherited_models(
-            model_name)
-        if inherited_models:
-            inherited_models_str = str(inherited_models)
-            if 'account.billable.item' in inherited_models:
-                resp = True
-        return resp
+        inherited_models = self.env["common.metadata"].get_inherited_models(model_name) or []
+        return "account.billable.item" in inherited_models
